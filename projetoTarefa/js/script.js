a72c488215b9672a21051ea2d06b0d3f1f0b8f91
Ana Fortes <miah.fortes@gmail.com>
2025-01-03 13:11:28 -0300

    algumas modificações

--- projetoTarefa/js/script.js
+++ projetoTarefa/js/script.js
@@ -12,12 +12,39 @@ clique.addEventListener('mouseover',function() {
 });
 
 //ao clicar
-clique.addEventListener('click', function() {
-    
+clique.addEventListener('click', function() {    
     this.style.border = '1px solid rgba(255, 255, 255, 0.94)';
     clicked = true;
+//criando elemento input
+    let form1 = document.createElement('input');
+    let botao1 = document.createElement('input');
+    let botao2 = document.createElement('input');
+//input do tipo texto
+    form1.setAttribute('type', 'text');
+//pegar o valor do input tarefa
+    let valorTarefa = tarefa.value;
+    //jogar o valor ao novo input
+    form1.value = valorTarefa;
+//especificando o tipo do input
+    botao1.setAttribute('type', 'button');
+    botao2.setAttribute('type', 'button');
+//criando os botoes com icones
+    botao1.innerHTML += '<span class="material-symbols-outlined">check</span>';
+    botao2.innerHTML += '<span class="material-symbols-outlined">close</span>';
+    //adicionando classe 
+    form1.classList.add('texto');
+    botao1.classList.add('icon');
+    botao2.classList.add('icon1');
+
+    let divs = document.querySelectorAll('div');
+    //especificando que sera na 4 div, que tem a posição 3
+    if(divs[3]) {
+        divs[3].appendChild(form1);
+        divs[3].appendChild(botao1);
+        divs[3].appendChild(botao2);
+    }
 //tempo para o botao voltar ao normal e removendo a class temporaria
-    setTimeout(function() {
+    setTimeout(() => {
         this.style.border = '';
         this.classList.remove('clicked');
     },500);
--- projetoTarefa/js/script2.js
+++
@@ -1,7 +0,0 @@
-/*
-variaveis globais:  
-tarefa 1 input
-clique 1 button submit
-
-caminho ao form 2, escondido que vai anotado na caixa -> optgroup -> label -> option
- */
