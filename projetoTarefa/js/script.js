430e2329bb9c27bfe6e7699447d466b0b59d0f00
Ana Fortes <miah.fortes@gmail.com>
2025-01-04 13:07:18 -0300

    algumas alterações
    preciso arrumar a posição dos botões

--- projetoTarefa/js/script.js
+++ projetoTarefa/js/script.js
@@ -1,5 +1,27 @@
 let tarefa = document.querySelector('#tarefa');
+let tarefas = document.querySelector('#tarefas')
 let clique = document.querySelector('#clique');
+
+function inputBotao() {
+//criando botao input
+    let botao1 = document.createElement('input');
+    let botao2 = document.createElement('input');
+//atribuindo o tipo
+    botao1.setAttribute('type', 'button');
+    botao2.setAttribute('type', 'button');
+//adicionando o valor do botao
+    botao1.value = '✔';
+    botao2.value = '✘'
+//criando uma classe ao botao
+    botao1.classList.add('icon');
+    botao2.classList.add('icon1');
+
+    botao1.style.cursor = 'pointer'
+    botao2.style.cursor = 'pointer'
+
+    return [botao1, botao2];
+    
+}
 
 //mouse dentro do limite do botao
 clique.addEventListener('mouseover',function() {
@@ -12,41 +34,35 @@ clique.addEventListener('mouseover',function() {
 });
 
 //ao clicar
-clique.addEventListener('click', function() {    
-    this.style.border = '1px solid rgba(255, 255, 255, 0.94)';
-    clicked = true;
-//criando elemento input
+clique.addEventListener('click', function () {    
+    //criando elemento input
     let form1 = document.createElement('input');
-    let botao1 = document.createElement('input');
-    let botao2 = document.createElement('input');
+    let divs = document.querySelectorAll('div');
+
+    this.style.border = '1px solid rgba(255, 255, 255, 0.94)';
+
 //input do tipo texto
     form1.setAttribute('type', 'text');
 //pegar o valor do input tarefa
-    let valorTarefa = tarefa.value;
-    //jogar o valor ao novo input
+    let valorTarefa = tarefas.value;
+//jogar o valor ao novo input
     form1.value = valorTarefa;
-//especificando o tipo do input
-    botao1.setAttribute('type', 'button');
-    botao2.setAttribute('type', 'button');
-//criando os botoes com icones
-    botao1.innerHTML += '<span class="material-symbols-outlined">check</span>';
-    botao2.innerHTML += '<span class="material-symbols-outlined">close</span>';
-    //adicionando classe 
-    form1.classList.add('texto');
-    botao1.classList.add('icon');
-    botao2.classList.add('icon1');
-
-    let divs = document.querySelectorAll('div');
-    //especificando que sera na 4 div, que tem a posição 3
+//adicionando classe 
+    form1.classList.add('texto'); 
+    
+    let [botao1, botao2] = inputBotao();
+//especificando que sera na 4 div, que tem a posição 3
     if(divs[3]) {
         divs[3].appendChild(form1);
         divs[3].appendChild(botao1);
-        divs[3].appendChild(botao2);
+        divs[3].appendChild(botao2)
     }
 //tempo para o botao voltar ao normal e removendo a class temporaria
     setTimeout(() => {
         this.style.border = '';
-        this.classList.remove('clicked');
+        this.classList.remove
+        ('clicked');
+
     },500);
 });
 
